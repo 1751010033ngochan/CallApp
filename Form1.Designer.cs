801a57ac9c68e894ac998737cca704ebafb5d54e
ngochan <1751010033han@ou.edu.vn>
2019-09-30 14:34:12 +0700

    Xu ly nut cong

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(80, 12);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(176, 22);
+            this.txtA.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Số a:";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(80, 66);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(176, 22);
+            this.txtB.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số b:";
+            // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(159, 158);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(75, 29);
+            this.btnCong.TabIndex = 2;
+            this.btnCong.Text = "Cộng";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(80, 114);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(176, 22);
+            this.txtKetQua.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kết quả:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(282, 199);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
+            this.Text = "Tính toán";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Label label3;
     }
 }
 
